6bf559d7d957304770cda9349e2195ef6500bc17
rijiprem <rijir.kmr@gmail.com>
2020-12-18 18:44:42 +0530

    added some more images

--- celebration.php
+++ celebration.php
@@ -9,7 +9,28 @@
 </head>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 <body>
-    
+<nav class="navbar navbar-expand-lg navbar-light bg-warning">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Chekaver</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="celebration.php">ഓണാഘോഷങ്ങള്‍</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="plays.php">ഓണക്കളികള്‍</a>
+              </li>
+              
+            </ul>
+          </div>
+        </div>
+      </nav>
     <div class="container">
         
 
@@ -24,12 +45,14 @@
                      ചിങ്ങത്തിലെ അത്തംനാൾ മുതലാ‍ണ് പൂക്കളം ഒരുക്കാൻ തുടങ്ങുന്നത്.
                     
                 </p>
+                <img src="images\Picture1.jpg" class="img-thumbnail" alt="...">
             </div>
             <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                 <h3>ഓണസദ്യ</h3>
                 <p>ഓണത്തിന്‍റെ പ്രധാന ആകര്‍ഷണം ഓണസദ്യ ആണ്. “ഉണ്ടറിയണം ഓണം “ എന്നാണ് വയ്പ്പ് . ആണ്ടിലൊരിക്കല്‍ പപ്പടവും ഉപ്പേരിയും കൂട്ടാനുള്ള അവസരമായിരുന്നു പണ്ടൊക്കെ സാധാരണക്കാരന് ഓണം . കാളന്‍, ഓലന്‍ ,എരിശ്ശേരി എന്നിവയാണ് ഓണസദ്യയില്‍ പ്രധാന വിഭവങ്ങള്‍.
 
                 </p>
+                <img src="images\Picture2.jpg" class="img-thumbnail" alt="...">
             </div>
         </div>
     </div>
--- plays.php
+++ plays.php
@@ -9,7 +9,28 @@
 </head>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 <body>
-    
+<nav class="navbar navbar-expand-lg navbar-light bg-warning">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Chekaver</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="celebration.php">ഓണാഘോഷങ്ങള്‍</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="plays.php">ഓണക്കളികള്‍</a>
+              </li>
+              
+            </ul>
+          </div>
+        </div>
+      </nav>
     <div class="container">
         
 
@@ -19,11 +40,13 @@
                 <h3>കൈകൊട്ടിക്കളി</h3>
                 <p>സ്‌ത്രീകളുടെ ഓണവിനോദങ്ങളിൽ പ്രഥമസ്ഥാനമാണ്‌ കൈകൊട്ടിക്കളിക്കുള്ളത്‌. പൊതുവെ എല്ലാ ജില്ലകളിലും കണ്ടുവരുന്ന ഒന്നാണിത്‌. വീടുകളുടെ അകത്തളങ്ങളുടെ സ്വകാര്യതകളിൽ നടത്തിപ്പോന്നിരുന്ന ഇത് പിൽകാലത്ത് മുറ്റത്തു പൂക്കളത്തിനു വലംവച്ചുകൊണ്ടും നടത്തിവരുന്നു. ഒരാൾ പാടുകയും മറ്റുള്ളവർ ഏറ്റുപാടുകയും ഒപ്പം വട്ടത്തിൽ നിന്ന്‌ ചുവടുവച്ച്‌ കൈകൊട്ടിക്കളിക്കുകയുമാണ്‌ പതിവ്‌. വൃത്താകൃതി ശ്രീബുദ്ധന്റെ ധർമ്മചക്രത്തെയാണ് സൂചിപ്പിക്കുന്നതെന്ന് ചില ചരിത്രകാരന്മാർ പറയുന്നു
                 </p>
+                <img src="images\Picture3.jpg" class="img-thumbnail" alt="...">
             </div>
             <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                 <h3>പുലികളി</h3>
                 <p>കേരളത്തിലെ തനതായ ഒരു കലാരൂപമാണ് പുലികളി അഥവാ കടുവകളി. ഓണക്കാലങ്ങളിലാണ് ഈ കലാരൂപം അവതരിപ്പിച്ച് വരാറുള്ളത്. ഈ കലാരൂപത്തിന് ഏകദേശം 200 വർഷത്തെ പഴക്കമുണ്ട്.. ഈ കലാരൂപം അവതരിപ്പിക്കുന്ന കലാകാരന്മാർ അന്നേദിവസം കടുവയുടെ ശരീരത്തിലുള്ളതു പോലുള്ള വരകളും, കടുവയുടെ മുഖവും ശരീരത്തിൽ വരയ്ക്കുകയും, മുഖത്ത് കടുവയുടെ മുഖം മൂടിയും വെച്ച് വാദ്യമേളങ്ങൾക്കനുസരിച്ച് നൃത്തം വെയ്ക്കുകയും ചെയ്യുന്നു. ഉടുക്കും, തകിലുമാണ് വാദ്യങ്ങളായി ഉപയോഗിക്കാറുള്ളത്. പുലികളെക്കൂടാതെ ഒരു വേട്ടക്കാരനും ഈ സംഘത്തിൽ ഉണ്ടായിരിക്കും. കടും മഞ്ഞ നിറത്തിലുള്ളതും, കറുപ്പ് നിറത്തിലുള്ളതുമായ ചായങ്ങളാണ് കൂടുതലായും വരയ്ക്കുവാൻ ഉപയോഗിക്കുന്നത്. പ്രത്യേകം പരിശീലനം സിദ്ധിച്ച ആളുകളാണ് ഈ കലാരൂപം അവതരിപ്പിക്കാറുള്ളത്.
                 </p>
+                <img src="images\Picture4.jpg" class="img-thumbnail" alt="...">
             </div>
         </div>
     </div>
